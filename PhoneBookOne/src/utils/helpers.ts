e473cd7571477f60eb9df95ba8318cd536418d0d
Daniel Morse <daniel.morse86@gmail.com>
2025-06-20 11:36:53 -0400

    helper for formatting names

--- PhoneBookOne/src/utils/helpers.ts
+++ PhoneBookOne/src/utils/helpers.ts
@@ -12,3 +12,24 @@ export const validatePhoneNumber = (phone: string): boolean => {
   const phoneRegex = /^(\+\d{1,2}\s)?\(?\d{3}\)?[\s.-]\d{3}[\s.-]\d{4}$/;
   return phoneRegex.test(phone);
 };
+
+export const getLastNameFormat = (user: User): string => {
+  let title = null;
+  let first = null;
+  let last = null;
+  const nameArray = user.name.split(" ");
+  if (
+    nameArray[0] === "Mr." ||
+    nameArray[0] === "Ms." ||
+    nameArray[0] === "Mrs."
+  ) {
+    title = nameArray[0];
+    nameArray.splice(0, 1);
+  }
+
+  first = nameArray[0];
+  nameArray.splice(0, 1);
+
+  last = nameArray.join(" ");
+  return `${last}, ${first} ${title ? `(${title})` : ""}`;
+};
